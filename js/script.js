0048f7b5513ea0f58ffa133fecb735fb45604c7f
Al Mamun <mamuncse82@gmail.com>
2022-10-16 13:15:02 +0600

    javascript_calculator

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// const number=[12, -15, 6 , -10, 9, -5, 5];
+// const text = "       The quick        brown fox jump over the lazy dog    ";
+
+
+// const array = number.filter(function(x) {return x > -1});
+// const array2 = number.filter(function(x) {return x<-1});
+// document.getElementById('filter').innerHTML ="The positive number: "+ array;
+// document.getElementById('filter2').innerHTML ="The positive number: "+ array2;
+// document.getElementById('space').innerHTML = text;
+
+
+// const count = text.match(/[aeiou]/gi).length;
+// document.getElementById('vowel').innerHTML ="Total Vowels are : " + count;
+ 
+
+// const cal = document.getElementById('calculator');
+
+
+// cal.addEventListener('submit', function(event){
+//     event.preventDefault();
+//     let f_value = document.getElementById('first_value').value;
+//     let s_value = document.getElementById('sec_value').value;
+//     let rslt = Number.parseInt(f_value) + Number.parseInt(s_value);
+//     document.getElementById('result').innerHTML = Number.parseInt(rslt);
+// })
